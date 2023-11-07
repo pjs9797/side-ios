48ca4281c06d953a7e59f4f73a31d9a852a37f0d
kms0524 <gcong2011@gmail.com>
2023-11-07 16:05:16 +0900

    [FEAT] Render SignIn view

---
+++ Projects/Feature/SignIn/Sources/SignInViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SignInViewController.swift
+//  FeatureSignIn
+//
+//  Created by 강민성 on 11/6/23.
+//
+
+import UIKit
+import Shared
+
+public class SignInViewController: UIViewController {
+    
+    var signInView = SignInView()
+    
+    public override func loadView() {
+        super.loadView()
+        view = signInView
+    }
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
